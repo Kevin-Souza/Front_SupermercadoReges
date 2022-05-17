68a88f8e1ee77eff455ad4f3d35ac083a677bd49
Kevin-Souza <kevinsouza1.ks@gmail.com>
2022-05-17 20:41:07 -0300

    add componentes e bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FrmClienteComponent } from './frm-cliente/frm-cliente.component';
+import { LstClienteComponent } from './lst-cliente/lst-cliente.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FrmClienteComponent,
+    LstClienteComponent
   ],
   imports: [
     BrowserModule,
