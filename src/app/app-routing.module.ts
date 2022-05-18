dd0fff3f6f2d9213bddf850a575a07fc1eaf2b85
Kevin-Souza <kevinsouza1.ks@gmail.com>
2022-05-17 21:41:19 -0300

    rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FrmClienteComponent } from './frm-cliente/frm-cliente.component';
+import { HomeComponent } from './home/home.component';
+import { LstClienteComponent } from './lst-cliente/lst-cliente.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", component: HomeComponent},
+  {path:"clientes", component: LstClienteComponent},
+  {path:"cliente", component: FrmClienteComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FrmClienteComponent } from './frm-cliente/frm-cliente.component';
 import { LstClienteComponent } from './lst-cliente/lst-cliente.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     FrmClienteComponent,
-    LstClienteComponent
+    LstClienteComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
